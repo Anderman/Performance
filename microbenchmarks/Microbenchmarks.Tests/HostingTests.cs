46e9abc337e7914d1df160ba230b7c8ae7808ed7
N. Taylor Mullen <nimullen@microsoft.com>
2015-12-01 12:17:24 -0800

    WIP hosting tests.

---
+++ microbenchmarks/Microbenchmarks.Tests/HostingTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Benchmarks.Framework;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+
+namespace Microbenchmarks.Tests
+{
+    public class HostingTests : BenchmarkTestBase
+    {
+        [Benchmark]
+        public void MainToConfigureOverhead()
+        {
+            var args = new string[0];
+
+            using (Collector.StartCollection())
+            {
+                TestStartup.Main(args);
+            }
+        }
+
+        private class TestStartup
+        {
+            public void Configure(IApplicationBuilder app)
+            {
+
+                // Stop
+            }
+
+            public static void Main(string[] args) => WebApplication.Run<TestStartup>(args);
+        }
+    }
+}
